3cfe04e9823eca37803dec7d0729f6a3bc8f936f
enriqueescobar-askida <enrique.escobar@askida.com>
2019-01-30 14:52:42 -0500

    Portable lib & console app

---
+++ StringProcessor/IStringProcessor.cs
@@ -0,0 +1,19 @@
+﻿namespace StringProcessor
+{
+    /// <summary>
+    /// The i string processor.
+    /// </summary>
+    public interface IStringProcessor
+    {
+        /// <summary>
+        /// The process.
+        /// </summary>
+        /// <param name="input">
+        /// The input.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        string Process(string input);
+    }
+}
---
+++ StringProcessor/ReverseWords.cs
@@ -0,0 +1,47 @@
+﻿namespace StringProcessor
+{
+    using System;
+
+    /// <summary>
+    /// The reverse words.
+    /// </summary>
+    public class ReverseWords : IStringProcessor
+    {
+        /// <summary>
+        /// The process.
+        /// </summary>
+        /// <param name="input">
+        /// The input.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public string Process(string input)
+        {
+            // We will use the original reverse processor
+            ReverseProcessor reverser = new ReverseProcessor();
+
+            // Split the string into words using space as the delimiter
+            string[] words = input.Split(' ');
+
+            // Go through the words and run each throught the processor.
+            // We use a for() loop because foreach() cannot modify the elements.
+            for (int i = 0; i < words.Length; i++)
+                words[i] = reverser.Process(words[i]);
+
+            // Turn it back into a single string.
+            return String.Join(" ", words);
+        }
+
+        /// <summary>
+        /// The to string.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public override string ToString()
+        {
+            return "Reverse the Words";
+        }
+    }
+}
